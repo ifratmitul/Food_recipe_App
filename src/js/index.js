fdb0129fe81e0a27a41d7fe2a804ecbd9ab6f402
ifratmitul <ifrat.mitul@hotmail.com>
2020-09-16 14:52:38 +0600

    recipe model implemented

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import Search from "./models/Search";
 import * as searchView from "./views/searchView";
 import { elements, renderLoader, clearLoader } from "./views/base";
+import Recipe from "./models/Recipe";
 const state = {};
 
 const controlSearch = async () => {
@@ -13,10 +14,14 @@ const controlSearch = async () => {
     searchView.clearResult();
     renderLoader(elements.searchRes);
 
-    await state.search.getResults();
-    // console.log(state.search.result);
-    clearLoader();
-    searchView.renderResults(state.search.result);
+    try {
+      await state.search.getResults();
+      // console.log(state.search.result);
+      clearLoader();
+      searchView.renderResults(state.search.result);
+    } catch (error) {
+      clearLoader();
+    }
   }
 };
 
@@ -38,5 +43,33 @@ elements.searchResPages.addEventListener("click", (e) => {
   //console.log(btn);
 });
 
-// const search = new Search("pizza");
-// console.log(search);
+//recipe controller
+
+// const r = new Recipe(47746);
+// r.getRecipe();
+
+const controlRecipe = async () => {
+  const id = window.location.hash.replace("#", "");
+  console.log(id);
+
+  if (id) {
+    state.recipe = new Recipe(id);
+    window.r = state.recipe;
+
+    try {
+      await state.recipe.getRecipe();
+      //console.log(state.recipe.ingredients);
+      state.recipe.parseIngredients();
+      state.recipe.calcTime();
+      state.recipe.calcServings();
+      //console.log(state.recipe);
+      //console.log(state);
+    } catch (err) {
+      alert(err);
+    }
+  }
+};
+
+["hashchange", "load"].forEach((Event) => {
+  window.addEventListener(Event, controlRecipe);
+});
